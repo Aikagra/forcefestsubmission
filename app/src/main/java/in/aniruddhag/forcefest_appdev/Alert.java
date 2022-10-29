3bb33fe1af7efed46130ad659e19cfeaf6aa77c3
Aniruddha22Gupta <Aniruddha2Gupta@gmail.com>
2022-10-29 23:06:58 +0530

    v1

--- app/src/main/java/in/aniruddhag/forcefest_appdev/Alert.java
+++ app/src/main/java/in/aniruddhag/forcefest_appdev/Alert.java
@@ -7,7 +7,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Alert extends AppCompatActivity {
+    public FirebaseAuth mAuth;
+    public FirebaseDatabase mDatabase;
+    public DatabaseReference databaseReference;
+    public FirebaseUser mUser;
 
     Activity activity;
     View view;
@@ -15,6 +24,13 @@ public class Alert extends AppCompatActivity {
         this.activity = activity;
         LayoutInflater li = activity.getLayoutInflater();
         view = li.inflate(R.layout.activity_alert, null);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance("https://forcefest-78f93-default-rtdb.asia-southeast1.firebasedatabase.app");
+        databaseReference = mDatabase.getReference();
+
+
 
         return view;
     }
