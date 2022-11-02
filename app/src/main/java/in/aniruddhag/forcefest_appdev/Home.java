82c866bc4bd1642535309b94056edc95d6871349
UwU <misterhecker696966@gmail.com>
2022-11-02 20:24:45 +0530

    final commit

--- app/src/main/java/in/aniruddhag/forcefest_appdev/Home.java
+++ app/src/main/java/in/aniruddhag/forcefest_appdev/Home.java
@@ -1,20 +1,26 @@
 package in.aniruddhag.forcefest_appdev;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Home extends AppCompatActivity {
 
     View view;
     Activity activity;
+    WebView webView;
 
     protected View onCreate(Activity activity) {
         this.activity = activity;
         LayoutInflater li = activity.getLayoutInflater();
         view = li.inflate(R.layout.activity_home, null);
+
+        webView = view.findViewById(R.id.webViewHome);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.ready.gov/kit");
 
         return view;
     }
